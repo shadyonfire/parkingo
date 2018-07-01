30aaca24038820e171a290893d46438850f3a4f0
shadyonfire <yaiamshailesh@gmail.com>
2018-07-01 08:43:13 +0530

    First Commit for splash screen

---
+++ app/src/main/java/com/example/shady/parkingo/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.example.shady.parkingo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
