b2ed48b1ebfff9cf17720f9287bc5ba84e79bbae
shadyonfire <yaiamshailesh@gmail.com>
2018-07-01 10:07:42 +0530

    Splash screen implemented and layout for Login page

--- app/src/main/java/com/example/shady/parkingo/SplashActivity.java
+++ app/src/main/java/com/example/shady/parkingo/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.example.shady.parkingo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent login=new Intent(SplashActivity.this,login_activity.class);
+                startActivity(login);
+                finish();
+            }
+        },400);
     }
 }
---
+++ app/src/main/java/com/example/shady/parkingo/login_activity.java
@@ -0,0 +1,13 @@
+package com.example.shady.parkingo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class login_activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
