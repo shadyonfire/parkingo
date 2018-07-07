911e75ef45f77ceb5acb5a937f179cf753fa1b7c
durgaprasadborra <durgaprasadborra1@gmail.com>
2018-07-07 10:30:58 +0530

    No parking toast added

--- app/src/main/java/com/example/shady/parkingo/currentParks.java
+++ app/src/main/java/com/example/shady/parkingo/currentParks.java
@@ -2,12 +2,16 @@ package com.example.shady.parkingo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,20 +21,26 @@ import static android.content.ContentValues.TAG;
 public class currentParks extends Activity {
     ListView curparks;
     ArrayList al;
+    LinearLayout ll;
     int[] ids={R.id.name,R.id.vehicle,R.id.slot,R.id.mobile};
     String[] keys={"name","vehicle","slot","mobile"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_parks);
-
+        ll=findViewById(R.id.ll);
 
         sqlite_ops so=new sqlite_ops(this);
         al=so.getAllFilledSlotDetails();
 
         curparks=findViewById(R.id.curparks);
-        SimpleAdapter sa=new SimpleAdapter(this,al,R.layout.current_park_listview,keys,ids);
-        curparks.setAdapter(sa);
+        if(al.size()>0){
+            SimpleAdapter sa=new SimpleAdapter(this,al,R.layout.current_park_listview,keys,ids);
+            curparks.setAdapter(sa);
+        }
+        else{
+            Toast.makeText(this,"Currently No Parking",Toast.LENGTH_SHORT  ).show();
+        }
 
         curparks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
